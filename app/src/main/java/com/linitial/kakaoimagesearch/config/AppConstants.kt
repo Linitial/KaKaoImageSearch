5532666647e43dff2180ec6c0ca238869d7503e4
Linitial <gsk5787@gmail.com>
2020-12-16 01:22:56 +0900

    feat: AppConstants 클래스 추가

---
+++ app/src/main/java/com/linitial/kakaoimagesearch/config/AppConstants.kt
@@ -0,0 +1,9 @@
+package com.linitial.kakaoimagesearch.config
+
+object AppConstants {
+
+    const val EMPTY_RESULT = "EMPTY_RESULT"
+    const val GRID_SPAN_SIZE = 3
+    const val LOAD_SPAN_SIZE = 1
+
+}
