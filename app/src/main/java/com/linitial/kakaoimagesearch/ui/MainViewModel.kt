b2a2073e76ba59566f8250c5ae00a7510a6f0030
Linitial <gsk5787@gmail.com>
2020-12-16 01:25:19 +0900

    feat: 뷰모델에 뷰 조절용 livedata 추가

--- app/src/main/java/com/linitial/kakaoimagesearch/ui/MainViewModel.kt
+++ app/src/main/java/com/linitial/kakaoimagesearch/ui/MainViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.paging.PagingData
-import com.linitial.kakaoimagesearch.data.imageSearch.repository.ImageInfo
+import com.linitial.kakaoimagesearch.data.imageSearch.repository.reponse.ImageInfo
 import com.linitial.kakaoimagesearch.data.imageSearch.repository.ImageSearchRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -18,19 +18,34 @@ class MainViewModel(
 
     private val compositeDisposable = CompositeDisposable()
 
-    private val _helloWorld = MutableLiveData<String>()
-    val helloWorld: LiveData<String> = _helloWorld
-
     private val _imageListData = MutableLiveData<PagingData<ImageInfo>>()
     val imageListData: LiveData<PagingData<ImageInfo>> = _imageListData
 
+    private val _hideKeyboard = MutableLiveData<Unit>()
+    val hideKeyboard = _hideKeyboard
+
+    private val _emptyResult = MutableLiveData<Boolean>()
+    val emptyResult = _emptyResult
+
+    private val _loadingStatus = MutableLiveData<Boolean>()
+    val loadingStatus = _loadingStatus
+
+    fun setEmptyList() {
+        _imageListData.value = PagingData.empty()
+    }
 
     fun searchImage(query: String) {
-        imageSearchRepository.searchImage(keyWord = query)
+        _hideKeyboard.value = Unit
+        _emptyResult.value = false
+        _loadingStatus.value = true
+
+        imageSearchRepository.searchImage(keyWord = query, "", 1, 10)
             .subscribeOn(Schedulers.computation())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeBy(
                 onNext = {
+                    _loadingStatus.value = false
+                    _emptyResult.value = false
                     _imageListData.value = it
                 },
                 onError = {
@@ -45,5 +60,9 @@ class MainViewModel(
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.dispose()
+    }
+
+    fun setEmptyResult() {
+        _emptyResult.value = true
     }
 }
