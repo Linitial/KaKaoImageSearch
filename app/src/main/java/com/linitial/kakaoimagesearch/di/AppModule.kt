71494bfea3c66762534c4a0e643eb4f3e0ffe7c0
Linitial <gsk5787@gmail.com>
2020-12-16 01:27:42 +0900

    feat: home 패키지 생성 및 내부로 클래스 이동

--- app/src/main/java/com/linitial/kakaoimagesearch/di/AppModule.kt
+++ app/src/main/java/com/linitial/kakaoimagesearch/di/AppModule.kt
@@ -3,7 +3,7 @@ package com.linitial.kakaoimagesearch.di
 import com.linitial.kakaoimagesearch.data.imageSearch.repository.ImageSearchRepository
 import com.linitial.kakaoimagesearch.data.imageSearch.repository.ImageSearchRepositoryImpl
 import com.linitial.kakaoimagesearch.network.KakaoApiProvider
-import com.linitial.kakaoimagesearch.ui.MainViewModel
+import com.linitial.kakaoimagesearch.ui.home.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
